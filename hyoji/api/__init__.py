13ac57520b006343833e335934051b5d699cfabf
Pierre-Nick Durette <pndurette@gmail.com>
2016-03-17 00:06:31 -0400

    Added (priliminary) standard api json reponse format

--- hyoji/api/__init__.py
+++ hyoji/api/__init__.py
@@ -1,7 +1,9 @@
 from hyoji import app
 from flask_restful import Api
+from flask import make_response
+import json
 
-# Flask Restfull init
+# Flask Restful init
 api = Api(app)
 
 # Import models that the API wll need
@@ -13,3 +15,11 @@ from .url import Url, UrlList
 # Register endpoints
 api.add_resource(Url,     '/url/<url_id>', '/url')
 api.add_resource(UrlList, '/urls')
+
+@api.representation('application/json')
+def output_json(data, code, headers=None):
+    """Standard api json reponse"""
+    #resp = make_response(json.dumps(data), code)
+    resp = make_response(json.dumps({'response': data, 'status': code}), code)
+    resp.headers.extend(headers or {})
+    return resp

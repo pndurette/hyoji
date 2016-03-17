e44e08e4122e8cc3e8e9240b60e022d943b41da2
Pierre-Nick Durette <pndurette@gmail.com>
2016-03-17 00:10:55 -0400

    Implemented PUT, DELETE for Url, output formatting, better validation

--- hyoji/api/url.py
+++ hyoji/api/url.py
@@ -1,10 +1,19 @@
 from flask import jsonify
-from flask_restful import Resource, abort, reqparse, inputs
+from flask_restful import Resource, abort, reqparse
+from flask_restful import inputs, fields, marshal_with
 from hyoji import models
 
+# Args
 parser = reqparse.RequestParser(bundle_errors=True)
-parser.add_argument('name', required=True)
+parser.add_argument('name')
 parser.add_argument('href', required=True, type=inputs.url)
+
+# Ouput formatting
+url_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'href':  fields.String
+}
 
 class Url(Resource):
     def get(self, url_id):
@@ -14,15 +23,33 @@ class Url(Resource):
             abort(404, message="url {} does not exist".format(url_id))
         return url.to_dict()
 
-    def put(self):
+    def post(self):
         args = parser.parse_args()
         q = models.Url.insert(name=args['name'], href=args['href'])
         new_id = q.execute()
         return self.get(new_id), 201
 
+    def put(self, url_id):
+        args = parser.parse_args()
+        try:
+            # If 'get' works then the row exists
+            # We then do an update
+            url = models.Url.get(models.Url.id == url_id)
+            q = models.Url.update(name=args['name'], href=args['href']).where(models.Url.id == url_id)
+            q.execute()
+        except models.Url.DoesNotExist:
+            # Otherwise, we do an insert
+            q = models.Url.insert(id=url_id, name=args['name'], href=args['href'])
+            new_id = q.execute()
+        return self.get(url_id), 201
+
+    def delete(self, url_id):
+        q = models.Url.delete().where(models.Url.id == url_id)
+        q.execute()
+        return "Deleted url {}".format(url_id) 
+
 class UrlList(Resource):
     def get(self):
-        # TODO: Guard against empty table
         q = models.Url.select()
         urls = []
         for url in q:

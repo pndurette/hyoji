36dea3274d33adbd38eee9d8653b49dedb758ce0
Pierre-Nick Durette <pndurette@gmail.com>
2016-03-18 22:43:37 -0400

    Split long lines, DELETE returns None

--- hyoji/api/url.py
+++ hyoji/api/url.py
@@ -32,21 +32,23 @@ class Url(Resource):
     def put(self, url_id):
         args = parser.parse_args()
         try:
-            # If 'get' works then the row exists
-            # We then do an update
+            # If 'get' doesn't raise then the
+            # row exists. We then do an update
             url = models.Url.get(models.Url.id == url_id)
-            q = models.Url.update(name=args['name'], href=args['href']).where(models.Url.id == url_id)
+            q = models.Url.update(name=args['name'], href=args['href'])\
+                          .where(models.Url.id == url_id)
             q.execute()
         except models.Url.DoesNotExist:
             # Otherwise, we do an insert
-            q = models.Url.insert(id=url_id, name=args['name'], href=args['href'])
+            q = models.Url.insert(id=url_id, name=args['name'],
+                                  href=args['href'])
             new_id = q.execute()
         return self.get(url_id), 201
 
     def delete(self, url_id):
         q = models.Url.delete().where(models.Url.id == url_id)
         q.execute()
-        return "Deleted url {}".format(url_id) 
+        return None 
 
 class UrlList(Resource):
     def get(self):

3e9d990a55c10c758b02fb4689816c8de3c239d8
Pierre-Nick Durette <pndurette@gmail.com>
2016-03-18 22:43:03 -0400

    General API JSON response format

--- hyoji/api/__init__.py
+++ hyoji/api/__init__.py
@@ -18,8 +18,30 @@ api.add_resource(UrlList, '/urls')
 
 @api.representation('application/json')
 def output_json(data, code, headers=None):
-    """Standard api json reponse"""
-    #resp = make_response(json.dumps(data), code)
-    resp = make_response(json.dumps({'response': data, 'status': code}), code)
+    """General API JSON response format"""
+    
+    # 'status' field
+    if http_status_is_bad(code):
+        status = 'error'
+        contents_label = 'message'
+        contents = data['message']
+    else:
+        status = 'success'
+        contents_label = 'data'
+        contents = data
+
+    # Response format
+    resp_dict = {
+        'status': status,
+        contents_label: contents
+    }
+
+    # Flask response
+    resp = make_response(json.dumps(resp_dict), code)
     resp.headers.extend(headers or {})
     return resp
+
+def http_status_is_bad(status_code):
+    """Returns true if `code` is [400,600["""
+    if 400 <= status_code < 600: return True
+    else: return False
